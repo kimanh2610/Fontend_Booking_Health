5123b5e02389ba7c4331abdce464ce85565b3ba2
HuynhKimAnh <huynhkimanhh2610@gmail.com>
2022-04-03 02:09:23 +0700

    crud(r) user b38

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -49,7 +49,7 @@ class Login extends Component {
             }
             if(data && data.errCode === 0) {
                 this.props.userLoginSuccess(data.user);
-                console.log('Login success')
+                //console.log('Login success')
             }
 
         } catch (error) {
--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,20 +1,71 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import './UserManage.scss';
+import { getAllUsers } from '../../services/userService';
 class UserManage extends Component {
 
-    state = {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            arrUsers: []
+        }
     }
 
-    componentDidMount() {
+    /** Life cycle
+     * Run component
+     * 1. Run construnct -> init state
+     * 2. Did mount (set state)
+     * 3. Render
+     */
 
+    async componentDidMount() {
+        let response = await getAllUsers('ALL');
+        if (response && response.errCode === 0) {
+            this.setState({
+                arrUsers: response.users
+            })
+
+        }
+        //console.log('get user from node.js :', response)
     }
 
 
     render() {
+        console.log('check render', this.state)
+        let arrUsers = this.state.arrUsers
         return (
-            <div className="text-center">Manage users</div>
+            <div className="user-container">
+                <div className="title text-center">Manage users</div>
+                <div className="users-table mt-3 mx-1">
+                    <table id="customers">
+                        <tr>
+                            <th>Email</th>
+                            <th>FullName</th>
+                            <th>Address</th>
+                            <th>Actions</th>
+                        </tr>
+
+                        {arrUsers && arrUsers.map((item, index) => {
+                            return (
+                                <tr>
+                                    <td>{item.email}</td>
+                                    <td>{item.fullName}</td>
+                                    <td>{item.address}</td>
+                                    <td>
+                                        <button className="btn-edit"><i className="fas fa-pencil-alt"></i></button>
+                                        <button className="btn-delete"><i className="fas fa-trash"></i></button>
+                                    </td>
+
+                                </tr>
+                            )
+                        })
+                        }
+
+
+                    </table>
+                </div>
+            </div>
         );
     }
 
