4d7eda98abc4f5479494801209f6fb83cbd2a377
HuynhKimAnh <huynhkimanhh2610@gmail.com>
2022-05-02 20:56:38 +0700

    create user authorization #71

--- src/containers/Header/menuApp.js
+++ src/containers/Header/menuApp.js
@@ -1,6 +1,6 @@
 export const adminMenu = [
     { //quản lý người dùng
-        name: 'menu.admin.manage-user', 
+        name: 'menu.admin.manage-user',
         menus: [
             {
                 name: 'menu.admin.crud', link: '/system/user-manage'
@@ -18,16 +18,20 @@ export const adminMenu = [
                 // ]
             },
 
-            {
-                name: 'menu.admin.manage-admin', link: '/system/user-admin'
-            },
+            // {
+            //     name: 'menu.admin.manage-admin', link: '/system/user-admin'
+            // },
 
+            { //quản lý kế hoạch khám bệnh
+
+                name: 'menu.doctor.manage-schedule', link: '/doctor/manage-schedule'
+            },
 
         ]
     },
 
     { //quản lý phòng khám
-        name: 'menu.admin.clinic', 
+        name: 'menu.admin.clinic',
         menus: [
             {
                 name: 'menu.admin.manage-clinic', link: '/system/manage-clinic'
@@ -36,7 +40,7 @@ export const adminMenu = [
     },
 
     { //quản lý chuyên khoa
-        name: 'menu.admin.specialty', 
+        name: 'menu.admin.specialty',
         menus: [
             {
                 name: 'menu.admin.manage-specialty', link: '/system/manage-specialty'
@@ -45,11 +49,24 @@ export const adminMenu = [
     },
 
     { //quản lý cẩm nang
-        name: 'menu.admin.handbook', 
+        name: 'menu.admin.handbook',
         menus: [
             {
                 name: 'menu.admin.manage-handbook', link: '/system/manage-handbook'
             },
         ]
     },
+];
+
+export const doctorMenu = [
+    {
+        name: 'menu.admin.manage-user',
+        menus: [
+
+            { //quản lý kế hoạch khám bệnh
+                name: 'menu.doctor.manage-schedule', link: '/doctor/manage-schedule'
+
+            },
+        ]
+    }
 ];
